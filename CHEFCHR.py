801b8599f28cfb6e52e12e7c57a8703979da97d7
Deepak Mecheri <ddeepakk13@gmail.com>
2018-02-06 21:29:24 +0530

    started working on CHEFCHR

---
+++ CHEFCHR.py
@@ -0,0 +1,11 @@
+T = int(input())
+chef = "chef"
+for _ in range(T):
+    s = input()
+    N = len(s)
+    i = 0                   #indexing variable
+
+    while i < N:
+        if s[i] in chef:
+            
+
